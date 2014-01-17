0c7dca343044888ca8d56d27201667442f3b7134
Matthew Dean <deanmatthew16@gmail.com>
2014-01-17 17:42:09 -0500

    Initial commit

---
+++ simple_available.js
@@ -0,0 +1,29 @@
+/*jslint browser: true, devel: true, plusplus: true, white: true */
+
+$(function() {
+	var url = window.location.href,
+		name = url.substr(url.indexOf("wiki/") + "wiki/".length),
+		apiUrl = "https://simple.wikipedia.org/w/api.php?action=query&titles=" + encodeURIComponent(name) + "&format=json";
+
+	if (name.indexOf(":") != -1) {
+		// Either a special or non-info page
+		return;
+	}
+
+	$.ajax({
+		url: apiUrl,
+		type: "GET",
+		success: function(result) {
+			if (!result.query.pages.hasOwnProperty('-1')) {
+				// The API did not return that the page had an index of -1 (not existing)
+				// Show the user that the page is available
+				$('#firstHeading.firstHeading').after('<a href="https://simple.wikipedia.org/wiki/' + name + '"><em>(simple available)</em></a>');
+			} else {
+				alert("Simple not available");
+			}
+		},
+		error: function(xhr, status, error) {
+			console.log("Error using API: " + error);
+		}
+	});
+});
